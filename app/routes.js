f1edea5634f21eb181544983f25fe6865d3b4afa
daniel-defra <daniel.dasilveira@defra.gov.uk>
2024-01-19 13:59:02 +0000

    content changes

--- app/routes.js
+++ app/routes.js
@@ -9,18 +9,18 @@ const router = govukPrototypeKit.requests.setupRouter()
 // Add your routes here
 
 // Run this code when a form is submitted to 'juggling-balls-answer'
-router.post('/new-edition/duplicate-answer.html', function (req, res) {
+router.post('/duplicate-form-answer', function (req, res) {
 
     // Make a variable and give it the value from 'how-many-balls'
-    var duplicateAnswer = req.session.data['duplicateAnswer']
+    var duplicateQuestion = req.session.data['duplicate-question']
 
     // Check whether the variable matches a condition
-    if (duplicateAnswer == "no"){
+    if (duplicateQuestion == "no"){
       // Send user to next page
       res.redirect('/name-of-form')
     } else {
       // Send user to ineligible page
-      res.redirect('/duplicate-answer')
+      res.redirect('/new-edition/choose-a-form.html')
     }
 
   })
